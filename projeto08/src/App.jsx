158bb79c2ab1dc265e24f8ea3b33a9965aef4cf6
eder64ghjhj <166666766+eder64ghjhj@users.noreply.github.com>
2024-10-26 18:41:26 -0300

    Add files via upload

---
+++ projeto08/src/App.jsx
@@ -0,0 +1,15 @@
+import React ,{ useState } from 'react'
+import ListaDeContatos from './components/ListaDeContatos'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+<ListaDeContatos/>
+      </>
+
+  )
+}
+
+export default App
